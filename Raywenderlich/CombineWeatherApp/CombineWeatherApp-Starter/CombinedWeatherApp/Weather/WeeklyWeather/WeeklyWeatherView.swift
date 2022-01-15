14dee5a3db75547d4eeb22cd102860dafb31b765
kamimi01 <awakaru0122@gmail.com>
2022-01-15 15:57:48 +0900

    feat: 天気の検索画面のview実装完了

--- Raywenderlich/CombineWeatherApp/CombineWeatherApp-Starter/CombinedWeatherApp/Weather/WeeklyWeather/WeeklyWeatherView.swift
+++ Raywenderlich/CombineWeatherApp/CombineWeatherApp-Starter/CombinedWeatherApp/Weather/WeeklyWeather/WeeklyWeatherView.swift
@@ -29,14 +29,63 @@
 import SwiftUI
 
 struct WeeklyWeatherView: View {
+  @ObservedObject var viewModel: WeeklyWeatherViewModel
+  
+  init(viewModel: WeeklyWeatherViewModel) {
+    self.viewModel = viewModel
+  }
+  
   var body: some View {
     NavigationView {
-      VStack {
-        NavigationLink(
-          "Best weather app :] ⛅️",
-          destination: CurrentWeatherView()
-        )
+      List {
+        searchField
+        
+        if viewModel.dataSource.isEmpty {
+          emptySection
+        } else {
+          cityHourlyWeatherSection
+          forecastSection
+        }
       }
+      .listStyle(GroupedListStyle())
+      .navigationBarTitle("Weather 🌥")
+    }
+  }
+}
+
+private extension WeeklyWeatherView {
+  var searchField: some View {
+    HStack(alignment: .center) {
+      // 1.
+      TextField("e.g. Cupertino", text: $viewModel.city)
+    }
+  }
+  
+  var forecastSection: some View {
+    Section {
+      // 2.
+      ForEach(viewModel.dataSource, content: DailyWeatherRow.init(viewModel:))
+    }
+  }
+  
+  var cityHourlyWeatherSection: some View {
+    Section {
+      NavigationLink(destination: CurrentWeatherView()) {
+        VStack(alignment: .leading) {
+          // 3.
+          Text(viewModel.city)
+          Text("Weather today")
+            .font(.caption)
+            .foregroundColor(.gray)
+        }
+      }
+    }
+  }
+  
+  var emptySection: some View {
+    Section {
+      Text("No results")
+        .foregroundColor(.gray)
     }
   }
 }
